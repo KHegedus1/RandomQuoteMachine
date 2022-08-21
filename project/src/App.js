c56f64702d622eb50dffa6acfbbdee6aba472b5f
KHegedus1 <kico.kiki@gmail.com>
2022-08-21 18:31:33 +0200

    import usestate,postavljanje

--- project/src/App.js
+++ project/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import Quotes from './Quotes'
 import './Style.css'; 
 
 function App() { 
+  const [quote, setQuote] = useState(second)
   return (
     <> 
     <Quotes />
