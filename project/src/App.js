3bf43ce2e83a54eb46aa88b8b22a5a02d4c57f1c
KHegedus1 <kico.kiki@gmail.com>
2022-08-21 18:18:54 +0200

    spajanje css-a na aplikaciju

--- project/src/App.js
+++ project/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Quotes from './Quotes'
+import './Style.css'; 
 
 function App() { 
   return (
     <> 
     <Quotes />
+    <div id ="qoute-box">
+      <div id="text"></div>
+      <div id="author"></div>
+      <div><button id="new-quote"></button></div>
+      <div><button><a id="tweet-quote">ssss</a></button></div>
+    </div>
     </>
   )
 }
