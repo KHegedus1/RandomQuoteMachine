305ef0344b12b54fa9f7d586ffd793fcf9f0fcc8
KHegedus1 <kico.kiki@gmail.com>
2022-08-21 17:44:15 +0200

    komentari

--- project/src/Quotes.js
+++ project/src/Quotes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Quotes from './Quotes'
 
-export default function quotes() { 
+export default function quotes() { //ime funkcije ne smije biti isto kao i ime datoteke
   return (
     <div>Quotes</div>
   )
