0135e30fce424192173f61ad9873e6fb5b73b964
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 15:31:11 +0200

    font awesome

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,15 @@
 import React ,{useState} from 'react';
-import Quotes from './Quotes'
+import Quotes from './Quotes';
 import './Style.css'; 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fas)
 
 function App() { 
   const [quote, setQuote] = useState("If the wind will not serve, take to the oars.")
   return (
     <> 
-    <Quotes />
     <div id ="qoute-box">
       <div id="text">{quote}</div>
       <div id="author"></div>
