fb5444684943f4ef85f7aece9604609f352ee79a
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 23:21:40 +0200

    Delete Quotes.js

--- project/src/Quotes.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Quotes from './Quotes'
-
-export default function quotes() { //ime funkcije ne smije biti isto kao i ime datoteke
-  return (
-    <div>Quotes</div>
-  )
-}
