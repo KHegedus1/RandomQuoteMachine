1a9a98f9ea861cfdc3c6c5af323151722d614094
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 15:33:08 +0200

    font awesome-free brands

--- project/src/App.js
+++ project/src/App.js
@@ -3,8 +3,10 @@ import Quotes from './Quotes';
 import './Style.css'; 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
-library.add(fas)
+library.add(fas, fab)
+
 
 function App() { 
   const [quote, setQuote] = useState("If the wind will not serve, take to the oars.")
