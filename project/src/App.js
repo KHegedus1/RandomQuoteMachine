ba2268a24a23728b98a78bfb12fe010cfacaa4dd
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 16:21:19 +0200

    react icons-twitter

--- project/src/App.js
+++ project/src/App.js
@@ -1,22 +1,19 @@
 import React ,{useState} from 'react';
 import Quotes from './Quotes';
 import './Style.css'; 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import {FaTwitter} from "react-icons/fa";
 
-library.add(fas, fab)
 
 
 function App() { 
   const [quote, setQuote] = useState("If the wind will not serve, take to the oars.")
   return (
-    <> 
+    <>
     <div id ="qoute-box">
       <div id="text">{quote}</div>
       <div id="author"></div>
       <button onClick={()=>setQuote("Citat 2 test")}>Change Quote</button>
-      <div><a id="tweet-quote"></a></div>
+      <div><a id="tweet-quote"><FaTwitter /></a></div>
     </div>
     </>
   )
