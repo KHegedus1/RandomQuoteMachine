f800a420ddd1ec50f32de0ec8e6f0e0152109b4f
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 19:25:27 +0200

    simetrija

--- project/src/App.js
+++ project/src/App.js
@@ -3,18 +3,21 @@ import Quotes from './Quotes';
 import './Style.css'; 
 import {FaTwitter} from "react-icons/fa"; 
 import {FaQuoteLeft} from "react-icons/fa";
+import {FaQuoteRight} from "react-icons/fa";
 
 
 function App() { 
-  const [quote, setQuote] = useState("If the wind will not serve, take to the oars.")
+  const [quote, setQuote] = useState("  If the wind will not serve, take to the oars.")
   const [author, setAuthor] = useState("test autor 1")
   return (
     <>
     <div id ="qoute-box">
-      <div id="text"><FaQuoteLeft />{quote}</div>
+      <div id="text"><FaQuoteLeft />{quote} <FaQuoteRight /></div>
       <div id="author">- {author}</div>
-      <button onClick={()=>setQuote("Citat 2 test")&setAuthor("autor 2 test")}>New Quote</button>
+      <div id="linija">
       <button><a id="tweet-quote"><FaTwitter /></a></button>
+      <button id ="newQuote" onClick={()=>setQuote(" Citat 2 test ")&setAuthor("autor 2555555555 test")}>New Quote</button>
+      </div>
     </div>
     <p id="credits">by KHegedus1</p> 
     </>
