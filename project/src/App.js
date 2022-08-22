cf487fc8b67634c1e90be091e8e04f786061ec2a
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 23:21:53 +0200

    refresh funkcija

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,4 @@
 import React ,{useState} from 'react';
-import Quotes from './Quotes';
 import './Style.css'; 
 import {FaTwitter} from "react-icons/fa"; 
 import {FaQuoteLeft} from "react-icons/fa";
@@ -9,6 +8,10 @@ import {FaQuoteRight} from "react-icons/fa";
 function App() { 
   const [quote, setQuote] = useState("  If the wind will not serve, take to the oars.")
   const [author, setAuthor] = useState("test autor 1")
+  const refreshQuoteAndAuthor = ()=>{setQuote(" Citat 2 test ");
+                                     setAuthor("autor 2555555555 test")}
+
+  
   return (
     <>
     <div id ="qoute-box">
@@ -16,7 +19,7 @@ function App() {
       <div id="author">- {author}</div>
       <div id="flex">
       <button id ="tweet-quote" ><a href='twitter.com/intent/tweet'><FaTwitter /></a></button>
-      <button id ="newQuote" onClick={()=>setQuote(" Citat 2 test ")&setAuthor("autor 2555555555 test")}>New Quote</button>
+      <button id ="newQuote" onClick={()=>refreshQuoteAndAuthor()}>New Quote</button>
       </div>
     </div>
     <p id="credits">by KHegedus1</p> 
