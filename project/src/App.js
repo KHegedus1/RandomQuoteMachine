c6ee7e573f97a08b7be0428a77df8e2ac550592a
KHegedus1 <kico.kiki@gmail.com>
2022-08-22 16:31:34 +0200

    twitter gumb

--- project/src/App.js
+++ project/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div id="text">{quote}</div>
       <div id="author"></div>
       <button onClick={()=>setQuote("Citat 2 test")}>Change Quote</button>
-      <div><a id="tweet-quote"><FaTwitter /></a></div>
+      <button><a id="tweet-quote"><FaTwitter /></a></button>
     </div>
     </>
   )
