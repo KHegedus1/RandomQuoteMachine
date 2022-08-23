73b1a3bc2103fca3e8ec32f326f880c9f8a7c223
KHegedus1 <kico.kiki@gmail.com>
2022-08-23 16:40:15 +0200

    rng-radi

--- project/src/App.js
+++ project/src/App.js
@@ -6,20 +6,26 @@ import {FaQuoteRight} from "react-icons/fa";
 
 
 function App() { 
-  const [quote, setQuote] = useState("  If the wind will not serve, take to the oars.")
-  const [author, setAuthor] = useState("test autor 1")
-  const refreshQuoteAndAuthor = ()=>{setQuote(" Citat 2 test ");
-                                     setAuthor("autor 2555555555 test")}
+  const [quote, setQuote] = useState("");
 
-  
+  const initialQuote = ()=>{
+    fetch("https://type.fit/api/quotes")
+    .then((res) =>res.json())
+    .then((data) => {
+      const rand = Math.floor(Math.random() *data.length )
+      setQuote(data[rand]);
+    });
+  };
+ 
+
   return (
     <>
     <div id ="qoute-box">
-      <div id="text"><FaQuoteLeft />{quote} <FaQuoteRight /></div>
-      <div id="author">- {author}</div>
+      <div id="text"><FaQuoteLeft />{quote.text} <FaQuoteRight /></div>
+      <div id="author">- {quote.author}</div>
       <div id="flex">
       <button id ="tweet-quote" ><a href='twitter.com/intent/tweet'><FaTwitter /></a></button>
-      <button id ="newQuote" onClick={()=>refreshQuoteAndAuthor()}>New Quote</button>
+      <button id ="newQuote" onClick={()=>initialQuote()}>New Quote</button>
       </div>
     </div>
     <p id="credits">by KHegedus1</p> 
