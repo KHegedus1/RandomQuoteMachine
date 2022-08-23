5326a99df5e09651c6e2991c52aa397f8647aa37
KHegedus1 <kico.kiki@gmail.com>
2022-08-23 17:22:36 +0200

    rng za pozadinu

--- project/src/App.js
+++ project/src/App.js
@@ -8,6 +8,7 @@ import {FaQuoteRight} from "react-icons/fa";
 function App() { 
   const [quote, setQuote] = useState("");
   const pozadina = ["#00FFFF","#faebd7","#7fffd4","#483d8b","#ff1493","#ffd700"];
+  const boje = useRef();
   const initialQuote = ()=>{
     fetch("https://type.fit/api/quotes")
     .then((res) =>res.json())
@@ -20,15 +21,18 @@ function App() {
   useEffect(() => {
     initialQuote(); 
   },[])
+  useEffect(() =>{
+    boje.current.style.color= pozadina[Math.floor(Math.random * pozadina.length)];
+  },[quote])
 
   return (
     <>
     <div id ="qoute-box">
-      <div id="text"><FaQuoteLeft />{quote.text} <FaQuoteRight /></div>
+      <div ref={boje} id="text"><FaQuoteLeft />{quote.text} <FaQuoteRight /></div>
       <div id="author">- {quote.author}</div>
       <div id="flex">
-      <button id ="tweet-quote" ><a href={`https://twitter.com/intent/tweet?text=${quote.text}`} target="_blank" rel="noopener noreferrer"><FaTwitter /></a></button>
-      <button id ="newQuote" onClick={()=>initialQuote()}>New Quote</button>
+      <button ref={boje} id ="tweet-quote" ><a href={`https://twitter.com/intent/tweet?text=${quote.text}`} target="_blank" rel="noopener noreferrer"><FaTwitter /></a></button>
+      <button ref={boje} id ="newQuote" onClick={()=>initialQuote()}>New Quote</button>
       </div>
     </div>
     <p id="credits">by KHegedus1</p> 
